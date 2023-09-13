ed35524fc74610add966adb2e683963f2b184a05
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-09-12 23:56:15 -0300

    try using fetch api

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { load } from 'cheerio'
-import puppeteer from 'puppeteer'
 import cors from 'cors'
 import { URL } from 'url'
 import { IProduct } from '../types/Product.types'
@@ -19,8 +18,16 @@ app.get('/products', async function(req, res) {
     const pichauUrl = new URL(`https://www.pichau.com.br/search?q=${slugify(searchItem ? searchItem : '')}`)
     const gkUrl = new URL(`https://www.gkinfostore.com.br/buscar?q=${slugify(searchItem ? searchItem : '')}`)
 
+/*    await getProducts(
+        kabumUrl,
+        '.productCard',
+        '.imageCard',
+        '.nameCard',
+        '.priceCard',
+        1
+    )*/
+
     await getProducts(
-        products,
         gkUrl,
         '.listagem-item',
         '.imagem-produto > img:first-child',
@@ -29,62 +36,50 @@ app.get('/products', async function(req, res) {
         3
     )
 
-    await getProducts(
-        products,
-        kabumUrl,
-        '.productCard',
-        '.imageCard',
-        '.nameCard',
-        '.priceCard',
-        1
-    )
-
-    await getProducts(
-        products,
+    /*await getProducts(
         pichauUrl,
         'a[data-cy="list-product"]',
         '.MuiPaper-root > div > div > div > img',
         'h2.MuiTypography-root',
         '.MuiCardContent-root > div > div:nth-child(1) > div > div:nth-child(3)',
         2
-    )
+    )*/
 
-    products.sort((a: IProduct, b: IProduct) => parseFloat(a.price.split('R$ ')[1]) > parseFloat(b.price.split('R$ ')[1]) ? 1: -1)
+    /*products.sort((a: IProduct, b: IProduct) => parseFloat(a.price.split('R$ ')[1]) > parseFloat(b.price.split('R$ ')[1]) ? 1: -1)*/
     res.header("Access-Control-Allow-Origin", "*")
     res.send(products)
+
+    async function getProducts(
+        searchUrl: any,
+        selector: string,
+        imageSelector:string,
+        titleSelector: string,
+        priceSelector: string,
+        idLoja: number
+    ) {
+        try {
+            await fetch(searchUrl)
+                .then((response) => {
+                    return response.text();
+                })
+                .then((html) => {
+                    const $ = load(html)
+
+                    $(selector).each((i, el) => {
+                        const image = $(imageSelector, el).attr('src')
+                        const title = $(titleSelector, el).text()
+                        const price = $(priceSelector, el).text()
+                        const link = 'https://www.kabum.com.br' + $('> a',el).attr('href')
+
+                        products.push({
+                            lojaId: idLoja, image, title, price, link
+                        })
+                    })
+                });
+        } catch (error: any) {
+            throw new Error(error);
+        }
+    }
 });
 
 app.listen(3000);
-
-async function getProducts(
-    products: IProduct[],
-    searchUrl: any,
-    selector: string,
-    imageSelector:string,
-    titleSelector: string,
-    priceSelector: string,
-    idLoja: number
-) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--headless']
-    })
-
-    const page = await browser.newPage()
-    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36");
-
-    await page.goto(searchUrl)
-    const html = await page.content()
-    const $ = load(html)
-
-    $(selector).each((i, el) => {
-        const image = $(imageSelector, el).attr('src')
-        const title = $(titleSelector, el).text()
-        const price = $(priceSelector, el).text()
-        const link = 'https://www.kabum.com.br' + $('> a',el).attr('href')
-
-        products.push({
-            lojaId: idLoja, image, title, price, link
-        })
-    })
-}
