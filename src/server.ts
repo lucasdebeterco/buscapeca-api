b458821cfd63d0c5d7666616b8dda0ef41149bf1
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-09-14 00:00:06 -0300

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -14,29 +14,29 @@ app.get('/products', async function(req, res) {
     const searchItem = req.query.searchItem && req.query.searchItem.toString()
     const products: IProduct[] = []
 
-    const kabumUrl = new URL(`https://www.kabum.com.br/busca/${slugify(searchItem ? searchItem : '')}`)
-    const pichauUrl = new URL(`https://www.pichau.com.br/search?q=${slugify(searchItem ? searchItem : '')}`)
-    const gkUrl = new URL(`https://www.gkinfostore.com.br/buscar?q=${slugify(searchItem ? searchItem : '')}`)
+    const kabumUrl = `https://www.kabum.com.br/busca/${slugify(searchItem ? searchItem : '')}`
+    const pichauUrl = `https://www.pichau.com.br/search?q=${slugify(searchItem ? searchItem : '')}`
+    const gkUrl = `https://www.gkinfostore.com.br/buscar?q=${slugify(searchItem ? searchItem : '')}`
 
-/*    await getProducts(
+    await getProducts(
         kabumUrl,
         '.productCard',
         '.imageCard',
         '.nameCard',
         '.priceCard',
         1
-    )*/
+    )
 
-    await getProducts(
+/*    await getProducts(
         gkUrl,
         '.listagem-item',
         '.imagem-produto > img:first-child',
         'a.nome-produto',
         '.desconto-a-vista',
         3
-    )
+    )*/
 
-    /*await getProducts(
+/*    await getProducts(
         pichauUrl,
         'a[data-cy="list-product"]',
         '.MuiPaper-root > div > div > div > img',
@@ -45,7 +45,7 @@ app.get('/products', async function(req, res) {
         2
     )*/
 
-    /*products.sort((a: IProduct, b: IProduct) => parseFloat(a.price.split('R$ ')[1]) > parseFloat(b.price.split('R$ ')[1]) ? 1: -1)*/
+    products.sort((a: IProduct, b: IProduct) => parseFloat(a.price.split('R$ ')[1]) > parseFloat(b.price.split('R$ ')[1]) ? 1: -1)
     res.header("Access-Control-Allow-Origin", "*")
     res.send(products)
 
@@ -58,24 +58,25 @@ app.get('/products', async function(req, res) {
         idLoja: number
     ) {
         try {
-            await fetch(searchUrl)
-                .then((response) => {
-                    return response.text();
-                })
-                .then((html) => {
-                    const $ = load(html)
+            await fetch(searchUrl, {
+                // @ts-ignore
+                method: 'GET', withCredentials: true, crossorigin: true, mode: 'no-cors',
+            }).then((response) => {
+                return response.text();
+            }).then((html: string) => {
+                const $ = load(html)
 
-                    $(selector).each((i, el) => {
-                        const image = $(imageSelector, el).attr('src')
-                        const title = $(titleSelector, el).text()
-                        const price = $(priceSelector, el).text()
-                        const link = 'https://www.kabum.com.br' + $('> a',el).attr('href')
+                $(selector).each((i, el) => {
+                    const image = $(imageSelector, el).attr('src')
+                    const title = $(titleSelector, el).text()
+                    const price = $(priceSelector, el).text()
+                    const link = 'https://www.kabum.com.br' + $('> a',el).attr('href')
 
-                        products.push({
-                            lojaId: idLoja, image, title, price, link
-                        })
+                    products.push({
+                        lojaId: idLoja, image, title, price, link
                     })
-                });
+                })
+            });
         } catch (error: any) {
             throw new Error(error);
         }
