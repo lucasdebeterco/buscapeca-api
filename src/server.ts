44ab4bfbd42ccb919012fded37c5ad295d050c18
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-11-15 18:33:14 -0300

    promisses refactor

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import routes from './routes'
 
-import { load } from 'cheerio'
 import { IProduct } from '../types/Product.types'
 import { slugify } from '../utils/slugify'
 import { getProducts } from './crawler/crawler'
@@ -18,12 +17,13 @@ app.use(cors())
 app.options('*', cors());
 
 app.get('/products', async function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*")
     const method  = req.method
     const searchItem = req.query.searchItem && req.query.searchItem.toString()
     const products: IProduct[] = []
 
-    const promisses = lojaData.map(async (loja) => {
-        return await getProducts(
+    await Promise.all(lojaData.map(async (loja) => {
+        await getProducts(
             loja.url + slugify(searchItem ? searchItem : ''),
             loja.cardSelector,
             loja.imageSelector,
@@ -31,19 +31,16 @@ app.get('/products', async function(req, res) {
             loja.priceSelector,
             loja.id,
             loja.lojaHost
-        )
-    })
+        ).then((productsResponse) => {
+            productsResponse.map(res => {
+                products.push(res)
+            })
+        })
+    }))
 
-    res.header("Access-Control-Allow-Origin", "*")
+    products.sort((a: IProduct, b: IProduct) => a.price > b.price ? 1: -1)
 
-    Promise.all(promisses).then((promissesResult) => {
-        promissesResult.map((targetResults) => targetResults.map(targetResult => {
-            products.push(targetResult)
-        }))
-
-        products.sort((a: IProduct, b: IProduct) => a.price > b.price ? 1: -1)
-        res.send(products)
-    })
+    res.send(products)
 });
 
 routes(app)
